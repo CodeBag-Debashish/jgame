f433eef7a8724d468d67febc989ced3ca9841b82
dopencv <dekadebashish3@gmail.com>
2017-04-13 21:23:20 +0530

    few chnages added main method

--- src/com/glc/swgame/Game.java
+++ src/com/glc/swgame/Game.java
@@ -4,5 +4,7 @@ public class Game {
 	public static int width = 300;
 	public static int height = width/ (16*9);
 	public static int scale = 3;
-	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
